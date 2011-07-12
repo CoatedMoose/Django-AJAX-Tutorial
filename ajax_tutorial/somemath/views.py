1682b954e2b04400a5ffb66ed4196068fb2fec47
Andrew Crichton <andrew.g.crichton@gmail.com>
2011-07-11 22:58:04 -0400

    Adding the view that can handle ajax.

--- ajax_tutorial/somemath/views.py
+++ ajax_tutorial/somemath/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.utils import simplejson
+    
+def ajax_example(request):
+    if not request.POST:
+        return render_to_response('weblog/ajax_example.html', {})
+    xhr = request.GET.has_key('xhr')
+    response_dict = {}
+    name = request.POST.get('name', False)
+    total = request.POST.get('total', False)
+    response_dict.update({'name': name, 'total': total})
+    if total:
+        try:
+            total = int(total)
+        except:
+            total = False
+    if name and total and int(total) == 10:
+        response_dict.update({'success': True})
+    else:
+        response_dict.update({'errors': {}})
+        if not name:
+            response_dict['errors'].update({'name': 'This field is required'})
+        if not total and total is not False:
+            response_dict['errors'].update({'total': 'This field is required'})
+        elif int(total) != 10:
+            response_dict['errors'].update({'total': 'Incorrect total'})
+    if xhr:
+        return HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+    return render_to_response('weblog/ajax_example.html', response_dict)
